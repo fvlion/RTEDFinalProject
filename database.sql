38ab7e37cdbd23c381d3abc0405176bdfd2dea76
Lion Feldman <lionfeldman@gmail.com>
2025-01-06 19:12:05 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE FinalProject;
+use FinalProject;
+
+CREATE TABLE numbers(
+    numbers_id int not null,
+    firstnumber int not null,
+    secondnumber int not null
+);
+insert into numbers_id(firstnumber, secondnumber)
+VALUES (1,2),(2,3),(3,4);
